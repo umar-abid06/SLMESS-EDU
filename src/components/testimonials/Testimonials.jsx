83066dbb8cc2f7cda75d8e031cc1a7b055f02313
Muhammad Umar Abid <mumarabid656@gmail.com>
2024-05-13 13:36:50 +0200

    Testimonials Slider

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Testimonials.css";
 import next_icon from "../../assets/next-icon.png";
 import back_icon from "../../assets/back-icon.png";
@@ -7,12 +7,45 @@ import user_2 from "../../assets/user-2.png";
 import user_3 from "../../assets/user-3.png";
 import user_4 from "../../assets/user-4.png";
 const Testimonials = () => {
+  const sliderRef = useRef();
+  let translateX = 0;
+
+  useEffect(() => {
+    const intervalId = setInterval(slideForward, 5000);
+
+    return () => {
+      // Cleanup function to clear the interval when component unmounts
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const slideForward = () => {
+    if (translateX > -50) {
+      translateX -= 25;
+    } else {
+      // Reset translateX to 0 if it reaches -50%
+      translateX = 0;
+    }
+    sliderRef.current.style.transform = `translateX(${translateX}%)`;
+  };
+  const slideBackward = () => {
+    if (translateX < 0) {
+      translateX += 25;
+    }
+    sliderRef.current.style.transform = `translateX(${translateX}%)`;
+  };
+
   return (
     <div className="testimonials">
-      <img src={next_icon} alt="" className="next-btn" />
-      <img src={back_icon} alt="" className="back-btn" />
+      <img src={next_icon} alt="" className="next-btn" onClick={slideForward} />
+      <img
+        src={back_icon}
+        alt=""
+        className="back-btn"
+        onClick={slideBackward}
+      />
       <div className="slider">
-        <ul>
+        <ul ref={sliderRef}>
           <li>
             <div className="slide">
               <div className="user-info">
@@ -21,15 +54,13 @@ const Testimonials = () => {
                   <h3>Tooba Shahid</h3>
                   <span>SLMESS, Karachi</span>
                 </div>
-                <div>
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Quasi expedita nulla ipsum deserunt totam suscipit autem
-                    modi facilis sit. Laudantium quam cum, velit sed minus non
-                    aliquid dolores atque in?
-                  </p>
-                </div>
               </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
+                expedita nulla ipsum deserunt totam suscipit autem modi facilis
+                sit. Laudantium quam cum, velit sed minus non aliquid dolores
+                atque in?
+              </p>
             </div>
           </li>
           <li>
@@ -40,15 +71,13 @@ const Testimonials = () => {
                   <h3>Rafi Memon</h3>
                   <span>SLMESS, Karachi</span>
                 </div>
-                <div>
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Quasi expedita nulla ipsum deserunt totam suscipit autem
-                    modi facilis sit. Laudantium quam cum, velit sed minus non
-                    aliquid dolores atque in?
-                  </p>
-                </div>
               </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
+                expedita nulla ipsum deserunt totam suscipit autem modi facilis
+                sit. Laudantium quam cum, velit sed minus non aliquid dolores
+                atque in?
+              </p>
             </div>
           </li>
           <li>
@@ -59,15 +88,13 @@ const Testimonials = () => {
                   <h3>Hira Bano</h3>
                   <span>SLMESS, Karachi</span>
                 </div>
-                <div>
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Quasi expedita nulla ipsum deserunt totam suscipit autem
-                    modi facilis sit. Laudantium quam cum, velit sed minus non
-                    aliquid dolores atque in?
-                  </p>
-                </div>
               </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
+                expedita nulla ipsum deserunt totam suscipit autem modi facilis
+                sit. Laudantium quam cum, velit sed minus non aliquid dolores
+                atque in?
+              </p>
             </div>
           </li>
           <li>
@@ -78,15 +105,13 @@ const Testimonials = () => {
                   <h3>Adeel Sheikh</h3>
                   <span>SLMESS, Karachi</span>
                 </div>
-                <div>
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Quasi expedita nulla ipsum deserunt totam suscipit autem
-                    modi facilis sit. Laudantium quam cum, velit sed minus non
-                    aliquid dolores atque in?
-                  </p>
-                </div>
               </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi
+                expedita nulla ipsum deserunt totam suscipit autem modi facilis
+                sit. Laudantium quam cum, velit sed minus non aliquid dolores
+                atque in?
+              </p>
             </div>
           </li>
         </ul>
